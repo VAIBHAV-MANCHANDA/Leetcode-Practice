a92681ee28561850461d4d7d9680ba25e9a54b7b
Vaibhav Manchanda <manchandavaibhav915@gmail.com>
2026-03-25 13:04:56 +0530

    method1

--- ApnaCollege/Anagram.java
+++ ApnaCollege/Anagram.java
@@ -1,3 +1,27 @@
+// race, care
+import java.util.*;
 public class Anagram {
-    
+    public static void main(String[] args){
+        String s1 ="care";
+        String s2="race";
+
+        if(method1(s1,s2))
+            System.out.print(("Anagram"));
+        else
+            System.out.print("Not anagram");
+    }
+
+    public static boolean method1(String s1, String s2){
+            
+        if(s1.length() != s2.length())
+            return false;
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1,arr2);
+    }
 }
